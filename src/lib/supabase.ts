6e660a93275622cbe66f2b6eefd752764472955c
mando03 <mando3103@icloud.com>
2025-12-18 21:22:01 -0600

    fix(supabase): 🐛 update environment variable for Supabase client

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,12 +1,12 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = import.meta.env.SUPABASE_URL;
-const supabaseKey = import.meta.env.SUPABASE_KEY;
+const supabaseServiceRoleKey = import.meta.env.SUPABASE_SERVICE_ROLE_KEY;
 
-if (!supabaseUrl || !supabaseKey) {
+if (!supabaseUrl || !supabaseServiceRoleKey) {
   throw new Error(
-    "SUPABASE_URL and SUPABASE_KEY must be defined in environment variables"
+    "SUPABASE_URL and SUPABASE_SERVICE_ROLE_KEY must be defined in environment variables"
   );
 }
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(supabaseUrl, supabaseServiceRoleKey);
